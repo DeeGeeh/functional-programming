20da96ed939bab163ede334d08a1c4340ab027bd
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-02-16 18:56:46 +0200

    done

--- week6/distance_functions_2.hs
+++ week6/distance_functions_2.hs
@@ -3,3 +3,5 @@ distanceFilter :: (String -> String -> Float) -> Float -> String -> [String] -> 
 
 distance3 x y = fromIntegral $ abs $ length x - length y
 
+distanceFilter f d s ss = 
+    filter (\x -> f s x <= d) ss
